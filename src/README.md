8dff91d09148992be0527ad21e4c3fc53d281c18
Felipe Mendes <lfelipecmendes@icloud.com>
2024-06-26 10:18:48 -0300

    feat: list exercise 01

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# Exercicío de Listas com Java
+
+---
+
+## Enunciado:
+
+Escreva um programa que receba um número inteiro *n* e depois os dados (id, nome e salario) de *n* funcionários (id's não podem se repetir).
+
+Em seguida efetuar o aumento de *x* % no salário de um funcionário com um determinado id. Ao final, mostrar a lista de funcionários com os dados atualizados.
---
+++ src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import entities.Employee;
+
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> employeeList = new ArrayList<>();
+
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Employee #" + i + ":");
+            System.out.print("Id: ");
+            Integer id = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Salary: ");
+            Double salary = sc.nextDouble();
+            employeeList.add(new Employee(id, name, salary));
+        }
+
+        System.out.print("Enter the employee id that will have salary increase: ");
+        Integer id = sc.nextInt();
+        Employee emp = employeeList.stream().filter(x -> Objects.equals(x.getId(), id)).findFirst().orElse(null);
+
+        if (emp == null) {
+            System.out.println("This id does not exist!");
+        } else {
+            System.out.print("Enter the percentage: ");
+            Double percentage = sc.nextDouble();
+            emp.increaseSalary(percentage);
+        }
+
+        System.out.println();
+        System.out.println("List of employees:");
+        for (Employee obj : employeeList) {
+            System.out.println(obj);
+        }
+
+        sc.close();
+    }
+}
